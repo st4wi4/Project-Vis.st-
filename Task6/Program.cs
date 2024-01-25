2d4864ab356a85c2115f39542d117dd94de38392
Nikita MArchenko <nikita.marchenko02@mail.ru>
2024-01-25 19:36:15 +0300

    add Lesson2: Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] arr = new int [5]; 
+arr[0] = 5; 
+arr[1] = 4; 
+arr[2] = 9; 
+arr[3] = 8; 
+arr[4] = 2; 
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+//Console.WriteLine(arr[5]);
